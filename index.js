d09ad36883b077a03e03d337ae72bf254ceb31b9
nntcuong <thecuongyt3htk@gmail.com>
2024-01-20 07:18:55 +0700

    gg

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
 
-fs.readFile('./files/starter.txt', (error, data) => {
+fs.readFile('./files/hello.txt', (error, data) => {
     if (error) {
         throw new Error(error.message);
     }
     console.log(data.toString());
 });
+process.on('uncaughtException',error => {
+    console.error(`There was an uncaught error: ${error}`);
+    process.exit(1);
+})
